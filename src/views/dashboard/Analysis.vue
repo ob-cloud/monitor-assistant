dc18b05dc0e10b93bfb65db5cc33ec7761e79ea9
eamiear <justcall_me_k@163.com>
2020-12-08 14:05:53 +0800

    导入配置

--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -3,7 +3,20 @@
     <a-card :bordered="false" class="card-area">
       <!-- 操作按钮区域 -->
       <div class="table-operator" style="margin-top: 5px">
-        <a-upload name="file" :showUploadList="false" :multiple="false" :headers="tokenHeader" :action="importExcelUrl" :beforeUpload="beforeUpload" @change="handleImportExcel">
+        <!-- <a-form layout="inline">
+          <a-form-item label="数据类型" :labelCol="labelCol" :wrapperCol="wrapperCol">
+            <a-radio-group v-model="type">
+              <a-radio value="panel">编组</a-radio>
+              <a-radio value="group">分组</a-radio>
+            </a-radio-group>
+          </a-form-item>
+          <a-form-item :labelCol="labelCol" :wrapperCol="wrapperCol" label="上传">
+            <a-upload name="file" :showUploadList="false" :multiple="false" :data="uploadData" :headers="tokenHeader" :action="importExcelUrl" :beforeUpload="beforeUpload" @change="handleImportExcel">
+              <a-button type="primary" icon="import">导入配置</a-button>
+            </a-upload>
+          </a-form-item>
+        </a-form> -->
+        <a-upload name="file" :showUploadList="false" :multiple="false" :data="uploadData" :headers="tokenHeader" :action="importExcelUrl" :beforeUpload="beforeUpload" @change="handleImportExcel">
           <a-button type="primary" icon="import">导入配置</a-button>
         </a-upload>
       </div>
@@ -30,8 +43,10 @@
 </template>
 
 <script>
+import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { ProListMixin } from '@/utils/mixins/ProListMixin'
 import { getRequestUrl } from '@/utils/ajax'
+import storage from 'store'
 export default {
   name: 'Analysis',
   mixins: [ProListMixin],
@@ -41,12 +56,27 @@ export default {
     return {
       url: {
         importExcelUrl: 'import/importPanelOrGroup'
+      },
+      type: 'panel',
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 8 }
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 18 }
       }
     }
   },
   computed: {
     importExcelUrl () {
       return getRequestUrl(this.url.importExcelUrl)
+    },
+    uploadData () {
+      return {
+        token: storage.get(ACCESS_TOKEN)
+        // type: this.type
+      }
     }
   },
   methods: {
@@ -55,7 +85,8 @@ export default {
     },
     beforeUpload (file) {
       var fileType = file.type
-      if (!fileType.includes('xls') || !fileType.includes('xlsx')) {
+
+      if (fileType.indexOf('sheet') < 0) {
         this.$message.warning('请上传 excel 文件')
         return false
       }
