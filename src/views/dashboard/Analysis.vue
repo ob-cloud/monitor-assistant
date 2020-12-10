2e23e1ab5730b5cd0a41204357dccfd61ae82a6d
eamiear <justcall_me_k@163.com>
2020-12-10 14:05:03 +0800

    api路径修改

--- src/views/dashboard/Analysis.vue
+++ src/views/dashboard/Analysis.vue
@@ -55,7 +55,7 @@ export default {
   data () {
     return {
       url: {
-        importExcelUrl: 'consumer/import/importPanelOrGroup'
+        importExcelUrl: '/consumer/import/importPanelOrGroup'
       },
       type: 'panel',
       labelCol: {
